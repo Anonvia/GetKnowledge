b9976c85aaad6374f3dbe2171f10e24e13548f15
Sanjaysheel <33994199+sanjaysheel@users.noreply.github.com>
2019-06-07 09:33:32 +0530

    first
    this is all file which are required to make this project

---
+++ Getknowledge/Home/all.js
@@ -0,0 +1,101 @@
+////.main-nav a {
+////
+////    color: #333;
+////    display: block;
+////    line-height: 64px;
+////
+////}
+////.main-nav a, .search-icon {
+////
+////    vertical-align: bottom;
+////    line-height: 55px;
+////
+////}
+////.main-nav-wrapper a {
+////
+////    color: #FFF;
+////    display: block;
+////    padding: 0 11px;
+////
+////}
+////.nav-open {
+////
+////    background: #EEE;
+////
+////}
+//a {
+//
+//    color: #2b6dad;
+//    text-decoration: none;
+//    -webkit-font-smoothing: antialiased;
+//    -moz-osx-font-smoothing: grayscale;
+//
+//}
+//a {
+//
+//    background-color: transparent;
+//
+//}
+//div, a {
+//
+//    transition: all .2s ease-in-out;
+//
+//}
+//.main-nav li {
+//
+//    text-transform: uppercase;
+//    font-size: 0.90em;
+//    list-style: none;
+//
+//}
+//.main-nav li {
+//
+//    text-transform: uppercase;
+//    font-size: 0.87em;
+//
+//}
+//.main-nav ul {
+//
+//    list-style: none;
+//
+//}
+//.main-nav {
+//
+//    color: #333;
+//    font-family: 'Open Sans',sans-serif;
+//    font-size: 17px;
+//
+//}
+//.main-nav-wrapper {
+//
+//    color: #FFF;
+//
+//}
+//
+//#tutorial-menu ul, #example-menu ul {
+//
+//    list-style: none;
+//
+//}
+//#tutorial-menu ul, #example-menu ul {
+//
+//    list-style: none;
+//
+//}
+//
+//#tutorial-menu ul, #example-menu ul {
+//
+//    list-style: none;
+//        list-style-position: outside;
+//        list-style-image: none;
+//        list-style-type: none;
+//
+//}
+//#tutorial-menu ul, #example-menu ul {
+//
+//    list-style: none;
+//        list-style-position: outside;
+//        list-style-image: none;
+//        list-style-type: none;
+//
+//} 
